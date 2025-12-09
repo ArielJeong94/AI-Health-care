4eedb9976a7c5bceb12842228cae0264056a7773
Ariel <jkh3043@gmail.com>
2025-12-09 16:30:01 +0900

    JavaScript 연습

---
+++ new_beginner/Beginner-track/index.js
@@ -0,0 +1,33 @@
+function countLength (string) {
+    return string.length;
+}
+let appleCount = countLength('사과');
+console. log(appleCount);
+let bananaCount = countLength('바나나');
+console. log(bananaCount);
+console. log(appleCount + bananaCount);
+
+let number = 2;
+console. log(number *1);
+console. log(number *2);
+console. log(number *3);
+console. log(number *4);
+console. log(number *5);
+console. log(number *6);
+console. log(number *7);
+console. log(number *8);
+console. log(number *9);
+
+function gugudan (number) {
+    console. log(number * 1);
+    console. log(number * 2);
+    console. log(number * 3);
+    console. log(number * 4);
+    console. log(number * 5);
+    console. log(number * 6);
+    console. log(number * 7);
+    console. log(number * 8);
+    console. log(number * 9);
+}
+gugudan(2);
+gugudan(3);
